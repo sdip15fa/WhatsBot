63ada9be395a743af103897bee20793604bcfc5a
wcyat <wcyat@wcyat.me>
2022-11-19 21:01:39 +0800

    processEvery: use default

--- helpers/agenda.ts
+++ helpers/agenda.ts
@@ -4,9 +4,7 @@ import db, { client } from "../db";
 import { wtsClient } from "../main";
 import { getDate } from "./date";
 
-export const agenda = new Agenda({ mongo: client.db("agenda") }).processEvery(
-  "30 seconds"
-);
+export const agenda = new Agenda({ mongo: client.db("agenda") });
 
 agenda.define("send count", async (job: Job) => {
   const { groupId } = job.attrs.data;
