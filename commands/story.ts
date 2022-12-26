c79b64d4888aff8679c7060f0021573371b3bd1b
wcyat <wcyat@wcyat.me>
2022-12-26 15:30:17 +0800

    story: remove, list, new; word limit;

--- commands/story.ts
+++ commands/story.ts
@@ -11,23 +11,42 @@ const execute = async (client: Client, msg: Message, args: string[]) => {
       if (!text) {
         return await client.sendMessage(chatId, "Please provide a word!");
       }
+      if (text.length > 15) {
+        return await client.sendMessage(
+          chatId,
+          "Word is too long (maximum is 15 characters)"
+        );
+      }
       if (
         !(
           await db("story").coll.updateOne(
             { chatId },
-            { $push: { story: text } }
+            { $push: { story: text }, $currentDate: { lastModified: true } }
           )
         ).matchedCount
       ) {
-        await db("story").coll.insertOne(<Story>{ chatId, story: [text] });
+        await db("story").coll.insertOne(<Story>{
+          chatId,
+          story: [text],
+          createdAt: new Date(),
+          lastModified: new Date(),
+        });
       }
-      await client.sendMessage(chatId, `Story updated: added ${text}`);
+      await client.sendMessage(
+        chatId,
+        `Story updated: added \`\`\`${text}\`\`\``
+      );
       break;
     }
     case "see": {
-      const story = (await db("story").coll.findOne({
-        chatId,
-      })) as Story | null;
+      const story = (
+        await db("story")
+          .coll.find({
+            chatId,
+          })
+          .sort({ createdAt: -1 })
+          .toArray()
+      )?.[Number(args[1]) || 0] as Story | null;
       if (story) {
         await client.sendMessage(
           chatId,
@@ -39,12 +58,76 @@ ${story.story.join(" ")}`
       }
       break;
     }
+    case "new": {
+      const text = args[1];
+      if (!text) {
+        return await client.sendMessage(chatId, "Please provide a word!");
+      }
+      if (text.length > 15) {
+        return await client.sendMessage(
+          chatId,
+          "Word is too long (maximum is 15 characters)"
+        );
+      }
+
+      await db("story").coll.insertOne(<Story>{
+        chatId,
+        story: [text],
+        createdAt: new Date(),
+        lastModified: new Date(),
+      });
+
+      await client.sendMessage(
+        chatId,
+        `New story created: \`\`\`${text}\`\`\``
+      );
+      break;
+    }
+    case "list": {
+      const stories = (await db("story")
+        .coll.find({ chatId })
+        .sort({ createdAt: -1 })
+        .toArray()) as Story[];
+      if (!stories.length) {
+        return await client.sendMessage(chatId, "No stories created yet");
+      }
+      await client.sendMessage(
+        chatId,
+        `Current stories:
+
+${stories
+  .map((story, index) => {
+    return `${index}:
+  Created: ${new Date(story.createdAt).toLocaleString("en-UK", {
+    timeZone: process.env.TZ,
+  })}
+  Last modified: ${new Date(story.createdAt).toLocaleString("en-UK", {
+    timeZone: process.env.TZ,
+  })}`;
+  })
+  .join("\n")}`
+      );
+      break;
+    }
+    case "remove": {
+      if (!msg.fromMe) {
+        return await client.sendMessage(chatId, "You cannot remove.");
+      }
+      await db("story").coll.updateOne(
+        {
+          chatId,
+        },
+        { $pop: { story: 1 } }
+      );
+      await client.sendMessage(chatId, "Last item removed.");
+      break;
+    }
     default: {
       await client.sendMessage(
         chatId,
         `Syntax:
-!story add [one word]
-!story see`
+\`\`\`!story add [one word]\`\`\`
+\`\`\`!story see\`\`\``
       );
       break;
     }
@@ -53,11 +136,11 @@ ${story.story.join(" ")}`
 
 module.exports = {
   name: "Story",
-  description: "Add words / view story",
+  description: "One word story",
   command: "!story",
   commandType: "plugin",
   isDependent: false,
-  help: `*Story*\n\nAdd words / view story.\n\n!story add [one word]\n\n!story see`,
+  help: `*Story*\n\n!story add [one word]\n\n!story new [one word]\n\n!story see [id]\n\n!story list\n\n!story remove`,
   execute,
   public: true,
 };
--- models/story.ts
+++ models/story.ts
@@ -1,7 +1,9 @@
 import { ObjectId } from "mongodb";
 
 export default interface Story {
-    _id?: ObjectId,
-    chatId: string,
-    story: string[]
+  _id?: ObjectId;
+  chatId: string;
+  createdAt: Date;
+  lastModified: Date;
+  story: string[];
 }
