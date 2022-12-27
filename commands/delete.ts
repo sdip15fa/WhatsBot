9da006afa45da6064cf059093130ee1be77f5699
wcyat <wcyat@wcyat.me>
2022-12-27 11:44:05 +0800

    delete

---
+++ commands/delete.ts
@@ -0,0 +1,31 @@
+import { Client, Message } from "whatsapp-web.js";
+
+const execute = async (client: Client, msg: Message, _args: string[]) => {
+  const chatId = (await msg.getChat()).id._serialized;
+  if (!msg.hasQuotedMsg) {
+    return await client.sendMessage(
+      chatId,
+      "Please reply to a message to delete."
+    );
+  }
+
+  const quote = await msg.getQuotedMessage();
+
+  try {
+    await msg.delete(true);
+    await quote.delete(true);
+  } catch {
+    await client.sendMessage(chatId, "Something went wrong while deleting.");
+  }
+};
+
+module.exports = {
+  name: "Delete",
+  description: "Delete a message for everyone",
+  command: "!delete",
+  commandType: "plugin",
+  isDependent: false,
+  help: `*Delete*\n\nDelete a message for everyone.\n\nReply a message with *!delete* to delete`,
+  execute,
+  public: false,
+};
