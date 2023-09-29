3a6408f0fe527583cc9b6eff696ee141636ff8ac
wcyat <wcyat@wcyat.me>
2023-09-30 01:36:59 +0800

    llama: try switching to rust backend

--- commands/llama.ts
+++ commands/llama.ts
@@ -1,12 +1,14 @@
 // Import necessary modules and dependencies
 import { LLM } from "llama-node";
-import { LLamaCpp } from "llama-node/dist/llm/llama-cpp.js";
+import { LLMRS } from "llama-node/dist/llm/llm-rs.js";
 import { Client, Message } from "whatsapp-web.js";
 import config from "../config.js";
+import { ModelType } from "@llama-node/core";
 
 const modelPath = config.llama_model_path; // Retrieve model path from environment variable
-const llama = new LLM(LLamaCpp);
-const llama_config = {
+const llama = new LLM(LLMRS);
+
+/*const llama_config = {
   modelPath, // Resolve model path based on the environment variable
   enableLogging: true,
   nCtx: 1024,
@@ -18,7 +20,7 @@ const llama_config = {
   embedding: false,
   useMmap: true,
   nGpuLayers: 0,
-};
+};*/
 
 const execute = async (client: Client, msg: Message, args: string[]) => {
   const chatId = (await msg.getChat()).id._serialized;
@@ -39,8 +41,16 @@ const execute = async (client: Client, msg: Message, args: string[]) => {
 
   const text = args.join(" ") || quotedMsg.body;
 
+  const prompt = `Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction:
+
+${text}
+
+### Response:`;
+
   // Call Llama model with the obtained text
-  const response = await llamaChat(text);
+  const response = await llamaChat(prompt);
 
   // Send the response back to the user
   await client.sendMessage(chatId, `Llama: ${response}`);
@@ -52,15 +62,18 @@ const execute = async (client: Client, msg: Message, args: string[]) => {
 
 // Function to interact with the Llama model
 const llamaChat = async (text: string) => {
-  await llama.load(llama_config);
+  await llama.load({
+    modelPath: config.llama_model_path,
+    modelType: ModelType.Llama /* ModelType.Llama */,
+  });
   let response = "";
   await llama.createCompletion(
     {
-      nThreads: 4,
-      nTokPredict: 2048,
+      numThreads: 4,
+      // nTokPredict: 2048,
       topK: 40,
       topP: 0.1,
-      temp: 0.2,
+      temperature: 0.2,
       repeatPenalty: 1,
       prompt: text,
     },
