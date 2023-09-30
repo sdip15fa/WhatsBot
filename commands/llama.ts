5f59dcf7eac21b4f764f8c97b7e740ce03829c44
wcyat <wcyat@wcyat.me>
2023-09-30 17:02:28 +0800

    fix(llama): use node-llama-cpp

--- commands/llama.ts
+++ commands/llama.ts
@@ -1,26 +1,13 @@
 // Import necessary modules and dependencies
-import { LLM } from "llama-node";
-import { LLMRS } from "llama-node/dist/llm/llm-rs.js";
 import { Client, Message } from "whatsapp-web.js";
 import config from "../config.js";
-import { ModelType } from "@llama-node/core";
+import { LlamaModel, LlamaContext, LlamaChatSession } from "node-llama-cpp";
 
 const modelPath = config.llama_model_path; // Retrieve model path from environment variable
-const llama = new LLM(LLMRS);
 
-/*const llama_config = {
-  modelPath, // Resolve model path based on the environment variable
-  enableLogging: true,
-  nCtx: 1024,
-  seed: 0,
-  f16Kv: false,
-  logitsAll: false,
-  vocabOnly: false,
-  useMlock: false,
-  embedding: false,
-  useMmap: true,
-  nGpuLayers: 0,
-};*/
+const model = new LlamaModel({ modelPath });
+const context = new LlamaContext({ model });
+const session = new LlamaChatSession({ context });
 
 const execute = async (client: Client, msg: Message, args: string[]) => {
   const chatId = (await msg.getChat()).id._serialized;
@@ -50,7 +37,7 @@ ${text}
 ### Response:`;
 
   // Call Llama model with the obtained text
-  const response = await llamaChat(prompt);
+  const response = await session.prompt(prompt);
 
   // Send the response back to the user
   await client.sendMessage(chatId, `Llama: ${response}`);
@@ -58,30 +45,6 @@ ${text}
   // Optionally, you can handle conversation history or context here
 
   // Optionally, update the last execution timestamp in your database
-};
-
-// Function to interact with the Llama model
-const llamaChat = async (text: string) => {
-  await llama.load({
-    modelPath: config.llama_model_path,
-    modelType: ModelType.Llama /* ModelType.Llama */,
-  });
-  let response = "";
-  await llama.createCompletion(
-    {
-      numThreads: 4,
-      // nTokPredict: 2048,
-      topK: 40,
-      topP: 0.1,
-      temperature: 0.2,
-      repeatPenalty: 1,
-      prompt: text,
-    },
-    (output: { token: string }) => {
-      response = output.token;
-    }
-  );
-  return response;
 };
 
 export default {
