1baa101f04cba803cf63d01be1fcf55b028e74d9
wcyat <wcyat@wcyat.me>
2023-12-28 01:54:16 +0800

    fix: replace urban-dictionary with ud-api

--- commands/ud.ts
+++ commands/ud.ts
@@ -1,5 +1,5 @@
 //jshint esversion:8
-import dictionary from "urban-dictionary";
+import dictionary from "ud-api";
 import { Client, Message } from "whatsapp-web.js";
 
 async function ud(term: string) {
@@ -17,7 +17,7 @@ const execute = async (client: Client, msg: Message, args: string[]) => {
     await client.sendMessage(
       chatId,
       `🙇‍♂️ *Error*\n\n` +
-        "```Something Unexpected Happened while Lookup on Urban Dictionary```"
+        "```Something Unexpected Happened while Lookup on Urban Dictionary```",
     );
   } else if (typeof data !== "string") {
     await client.sendMessage(
@@ -30,7 +30,7 @@ const execute = async (client: Client, msg: Message, args: string[]) => {
         "```\n\n" +
         "*Example:* ```" +
         data[0].example +
-        "```"
+        "```",
     );
   }
 };
