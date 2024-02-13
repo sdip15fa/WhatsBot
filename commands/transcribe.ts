5056d22a60487c3f974f220f6c2fe7c4767f46bf
wcyat <wcyat@wcyat.me>
2024-02-13 23:40:13 +0800

    fix: transcribe

--- commands/transcribe.ts
+++ commands/transcribe.ts
@@ -1,6 +1,5 @@
 //jshint esversion:8
 import whatsapp, { Client, Message } from "whatsapp-web.js";
-import { client as gradio } from "@gradio/client";
 
 const execute = async (client: Client, msg: Message) => {
   const chatId = (await msg.getChat()).id._serialized;
@@ -19,15 +18,26 @@ const execute = async (client: Client, msg: Message) => {
         `🙇‍♂️ *Error*\n\n` + "```Media is not audio```",
       );
     }
-    const app = await gradio("https://openai-whisper.hf.space/", {});
-    const result = (await app.predict("/predict", [
-      attachmentData.data, // blob in 'inputs' Audio component
-      "transcribe", // string  in 'Task' Radio component
-    ])) as { data: string[] };
-    await client.sendMessage(
-      chatId,
-      `*Transcription*\n\n` + result.data?.[0] || "",
-    );
+
+    const result = (await fetch(
+      "https://api-inference.huggingface.co/models/wcyat/whisper-small-yue-mdcc",
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; rv:122.0) Gecko/20100101 Firefox/122.0",
+          Accept: "*/*",
+          "Accept-Language": "en-US,en;q=0.5",
+          "content-type": attachmentData.mimetype,
+          "x-wait-for-model": "true",
+        },
+        referrer: "https://huggingface.co/",
+        method: "POST",
+        mode: "cors",
+        body: Buffer.from(attachmentData.data, "base64"),
+      },
+    ).then((res) => res.json())) as { text: string };
+
+    await client.sendMessage(chatId, `*Transcription*\n\n` + result.text || "");
   } else {
     await client.sendMessage(chatId, `🙇‍♂️ *Error*\n\n` + "```No media found```");
   }
