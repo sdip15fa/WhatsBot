223a08e624610e4d74238058614c0c0d4ec49235
wcyat <wcyat@wcyat.me>
2024-02-14 19:25:42 +0800

    cf-worker: modularize

---
+++ cf-worker/src/exceptions.ts
@@ -0,0 +1,23 @@
+export class UnauthorizedException {
+	status: number;
+	statusText: string;
+	reason: string;
+
+	constructor(reason: string) {
+		this.status = 401;
+		this.statusText = 'Unauthorized';
+		this.reason = reason;
+	}
+}
+
+export class BadRequestException {
+	status: number;
+	statusText: string;
+	reason: string;
+
+	constructor(reason: string) {
+		this.status = 400;
+		this.statusText = 'Bad Request';
+		this.reason = reason;
+	}
+}
---
+++ cf-worker/src/types.ts
@@ -0,0 +1,6 @@
+export interface Env {
+	AUTH_USER: string;
+	AUTH_PASSWORD: string;
+	AI: string;
+	MONGO_APP_ID: string;
+}
