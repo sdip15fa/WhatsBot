2a94d273079a142690a169a7d499bc9f7e2b46b4
wcyat <wcyat@wcyat.me>
2024-02-16 17:17:43 +0800

    fix(llama): system prompt

--- cf-worker/src/index.ts
+++ cf-worker/src/index.ts
@@ -68,8 +68,7 @@ export default {
 					messages: [
 						{
 							role: 'system',
-							content:
-								"Below are a series of dialogues between various people and an AI assistant. The AI tries to be helpful, polite, honest, sophisticated, emotionally aware, and humble-but-knowledgeable. The assistant is happy to help with almost anything, and will do its best to understand exactly what is needed. It also tries to avoid giving false or misleading information, and it caveats when it isn't entirely sure about the right answer. That said, the assistant is practical and really does its best, and doesn't let caution get too much in the way of being useful.",
+							content: 'Below is an instruction that describes a task. Write a response that appropriately completes the request.',
 						},
 						...messages,
 					],
