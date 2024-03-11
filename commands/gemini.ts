4f131d4933e8320dea929cb02e16cdf9540847aa
wcyat <wcyat@wcyat.me>
2024-03-11 21:59:05 +0800

    fix: gemini

--- commands/gemini.ts
+++ commands/gemini.ts
@@ -77,28 +77,6 @@ const execute = async (client: Client, msg: Message, args: string[]) => {
 
     const chat = model.startChat({
       history,
-      safetySettings: [
-        {
-          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
-          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-      ],
     });
 
     const result = await chat.sendMessage(prompt);
