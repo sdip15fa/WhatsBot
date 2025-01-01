aa256108e07777a8237082a63a9ada28fe2c28fa
wcyat <wcyat@wcyat.me>
2025-01-01 14:23:30 +0800

    feat(gemini): add web search

--- commands/gemini.ts
+++ commands/gemini.ts
@@ -3,6 +3,7 @@ import { Client, Message } from "whatsapp-web.js";
 import { Command } from "../types/command.js";
 import config from "../config.js";
 import {
+  DynamicRetrievalMode,
   GoogleGenerativeAI,
   HarmBlockThreshold,
   HarmCategory,
@@ -22,8 +23,13 @@ const execute = async (client: Client, msg: Message, args: string[]) => {
     return client.sendMessage(chatId, "Please provide prompt to gemini!");
   }
 
+  const searchWithGoogle = args[0] === "google";
+  if (searchWithGoogle) {
+    args.shift();
+  }
+
   const prompt = args.join(" ") || quotedMsg.body;
-  const history: { role: "model" | "user"; parts: string }[] = [];
+  const history: { role: "model" | "user"; parts: { text: string }[] }[] = [];
 
   if (
     args.length &&
@@ -37,10 +43,19 @@ const execute = async (client: Client, msg: Message, args: string[]) => {
         currMsg.body.startsWith("Gemini:") && currMsg.fromMe ? "model" : "user";
       history.unshift({
         role,
-        parts:
-          role === "model"
-            ? currMsg.body.replace("Gemini: ", "")
-            : currMsg.body.trim().slice(1).trim().replace("gemini", "").trim(),
+        parts: [
+          {
+            text:
+              role === "model"
+                ? currMsg.body.replace("Gemini: ", "")
+                : currMsg.body
+                    .trim()
+                    .slice(1)
+                    .trim()
+                    .replace("gemini", "")
+                    .trim(),
+          },
+        ],
       });
       if (currMsg.hasQuotedMsg) {
         const nextQuotedMsg = await currMsg.getQuotedMessage();
@@ -73,7 +88,39 @@ const execute = async (client: Client, msg: Message, args: string[]) => {
 
   try {
     const genAI = new GoogleGenerativeAI(config.gemini_api_key);
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const model = genAI.getGenerativeModel({
+      model: "gemini-pro",
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+      ],
+      ...(searchWithGoogle && {
+        tools: [
+          {
+            googleSearchRetrieval: {
+              dynamicRetrievalConfig: {
+                mode: DynamicRetrievalMode.MODE_DYNAMIC,
+                dynamicThreshold: 0.7,
+              },
+            },
+          },
+        ],
+      }),
+    });
 
     const chat = model.startChat({
       history,
